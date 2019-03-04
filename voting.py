8b1145b55a29ffa157e009d68c07ff530f846442
Kyrylo Kukhelnyi <mrkiril@ukr.net>
2019-03-04 21:16:10 +0200

    style result

--- voting.py
+++ voting.py
@@ -6,17 +6,18 @@ request_voting = requests.get("https://gromrada.mva.gov.ua/voting/public/public_
 request_candidate = requests.get("https://gromrada.mva.gov.ua/candidates/candidates.json?fbclid=IwAR15LYLz7A249-qhO4C3F6fhUuq351fn1qT5p6K3rcpMmGRHuF7cJVwA-aE")
 candidates_json = request_candidate.json()
 candidates = {str(c["id"]): c["name"] for c in candidates_json['candidates']}
-print candidates
 
 
 votes = defaultdict(list)
-for vote in request_voting.content.split("\r\n"):
+for vote in request_voting.content.decode().split("\r\n"):
     if vote != "":
-        str_candidates = re.search("SEL=([\d,]+)", vote).group(1)
+        str_candidates = re.search(r"SEL=([\d,]+)", vote).group(1)
         for c in str_candidates.split(","):
             votes[c].append(1)
 
 votes = {candidate: len(votes) for candidate, votes in votes.items()}
 votes = sorted(votes.items(), key=lambda kv: kv[1], reverse=True)
-for c, v in votes:
-    print u"{name:38} >> {vote} ".format(name=candidates[str(c)], vote=v)
+for i, (c, v) in enumerate(votes):
+    print(
+        "{iter:<5} {name:38} >>   {vote} ".format(iter=str(i+1)+".", name=candidates[str(c)], vote=v)
+    )
